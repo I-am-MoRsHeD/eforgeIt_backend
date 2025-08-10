5c8bbad0c62e90852b83d8ac58af897f572a5299
MoRsHeD <mdmorshed0187@gmail.com>
2025-08-10 18:15:03 +0600

    changed admin mail in env

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ app.use(cors());
 app.use('/api/v1', router);
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Welcome to Tour Management App!!');
+    res.send('Welcome to School Assessment Test App!!');
 });
 
 
